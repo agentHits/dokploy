ed51085ede941a7f26122fc58ad2eb6c1e196c82
agentHits <140916359+agentHits@users.noreply.github.com>
2026-06-08 23:22:31 +0300

    feat: add safe application env upsert

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -101,6 +101,7 @@ export * from "./utils/docker/compose/volume";
 export * from "./utils/docker/domain";
 export * from "./utils/docker/types";
 export * from "./utils/docker/utils";
+export * from "./utils/env-upsert";
 export * from "./utils/filesystem/directory";
 export * from "./utils/filesystem/safe-path";
 export * from "./utils/filesystem/ssh";
